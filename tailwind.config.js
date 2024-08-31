833b322acead7623cf85a879d47f7ce27e72d8ef
dev-vilela <paulov4@hotmail.com>
2024-08-31 13:45:40 -0300

    at

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      'sans': ['Roboto','sans-serif']
+    },
+
     extend: {
       backgroundImage:{
         "home": "url('/assets/bg.png')"
